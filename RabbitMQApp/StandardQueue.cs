0c4d4141497026971a1c63770d237d249e2c5cca
bgreer5050@ <bgreer5050@gmail.com>
2018-10-06 22:17:00 -0500

    Added StandardQueue class with CreateQueue method.

---
+++ RabbitMQApp/StandardQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ;
+using RabbitMQ.Client;
+
+namespace RabbitMQApp
+{
+    public static class StandardQueue
+    {
+        private const string QueueName = "StandardQueue_ExampleQueue";
+
+        private static void CreateQueue()
+        {
+            var _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            IConnection  connection = _factory.CreateConnection();
+            IModel _model = connection.CreateModel();
+            _model.QueueDeclare(QueueName, false, false, false, null);
+            
+        }
+
+
+    }
+}
