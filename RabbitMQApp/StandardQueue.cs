36620e719341b42a559626c59487d78aab4b6a88
bgreer5050@ <bgreer5050@gmail.com>
2018-10-06 23:08:27 -0500

    Added SendMessage to StandardQueue class

--- RabbitMQApp/StandardQueue.cs
+++ RabbitMQApp/StandardQueue.cs
@@ -11,6 +11,7 @@ namespace RabbitMQApp
     public static class StandardQueue
     {
         private const string QueueName = "StandardQueue_ExampleQueue";
+        private static IModel _model;
 
         private static void CreateQueue()
         {
@@ -22,11 +23,21 @@ namespace RabbitMQApp
             };
 
             IConnection  connection = _factory.CreateConnection();
-            IModel _model = connection.CreateModel();
+            _model = connection.CreateModel();
             _model.QueueDeclare(QueueName, false, false, false, null);
             
         }
 
+        private static void SendMessage(Payment message)
+        {
+            //Leaving the exchange blank. Publish will use the default exchange
+            _model.BasicPublish("", QueueName, null, message.Serialize());
 
+            Console.WriteLine(" Payment message sent:{0} :{1}",
+                                message.CardNumber,
+                                message.AmountToPay);
+        }
     }
+
+    
 }
