9b84b70a9cf0f2b7f3c0902c759e4cd0807b9508
bgreer5050@ <bgreer5050@gmail.com>
2018-10-08 08:49:48 -0500

    Changed the StandardQueue to durable which means the Queue will survive a server restart.  This DOES NOT MEAN the messages will!

--- RabbitMQApp/StandardQueue.cs
+++ RabbitMQApp/StandardQueue.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using RabbitMQ;
 using RabbitMQ.Client;
+using Common;
 
 namespace RabbitMQApp
 {
@@ -24,18 +25,21 @@ namespace RabbitMQApp
 
             IConnection  connection = _factory.CreateConnection();
             _model = connection.CreateModel();
-            _model.QueueDeclare(QueueName, false, false, false, null);
+            _model.QueueDeclare(QueueName, true, false, false, null);
             
         }
 
         private static void SendMessage(Payment message)
         {
-            //Leaving the exchange blank. Publish will use the default exchange
+            // Leaving the exchange blank. Publish will use the default exchange
+            // The message will be published to the StandardQueue_ExampleQueue queue
+            // The third parameter is if we want to pass some properties along with the message (i.e. correlation Id, reply to addresses, etc
+            //Finally we take our message and serialize it into a byte array using the extension method we created in our Object Serialize class.
             _model.BasicPublish("", QueueName, null, message.Serialize());
 
             Console.WriteLine(" Payment message sent:{0} :{1}",
                                 message.CardNumber,
-                                message.AmountToPay);
+                                message.AmounToPay);
         }
     }
 
