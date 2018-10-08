086afe10b3be4f639178335eac2dadc64ddd6ce5
bgreer5050@ <bgreer5050@gmail.com>
2018-10-08 15:02:58 -0500

    Publish Subscribe is working

--- PublishSubscribe_Publisher/Program.cs
+++ PublishSubscribe_Publisher/Program.cs
@@ -1,15 +1,65 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 
 namespace PublishSubscribe_Publisher
 {
     class Program
     {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        private const string ExchangeName = "PublishSubscribe_Exchange";
+
         static void Main(string[] args)
         {
+            var payment1 = new Payment { AmounToPay = 250.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            var payment2 = new Payment { AmounToPay = 25.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            var payment3 = new Payment { AmounToPay = 12.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            var payment4 = new Payment { AmounToPay = 84.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            var payment5 = new Payment { AmounToPay = 3.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            var payment6 = new Payment { AmounToPay = 19.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            var payment7 = new Payment { AmounToPay = 16.7m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            var payment8 = new Payment { AmounToPay = 33.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            var payment9 = new Payment { AmounToPay = 29.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            var payment10 = new Payment { AmounToPay = 8.40m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+
+            CreateConnection();
+
+            SendMessage(payment1);
+            SendMessage(payment2);
+            SendMessage(payment3);
+            SendMessage(payment4);
+            SendMessage(payment5);
+            SendMessage(payment6);
+            SendMessage(payment7);
+            SendMessage(payment8);
+            SendMessage(payment9);
+            SendMessage(payment10);
+
+            Console.ReadLine();
         }
+
+        private static void CreateConnection()
+        {
+            _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" }; //Create a connection factory
+            _connection = _factory.CreateConnection(); //Create a connection
+            _model = _connection.CreateModel();
+
+            //_model.QueueDeclare(QueueName, true, false, false, null);  //If it does not already exist, RabbitMQ will create a queue name after our QueueName variable
+            _model.ExchangeDeclare(ExchangeName, "fanout", false);
+        }
+
+        private static void SendMessage(Payment message)
+        {
+            _model.BasicPublish(ExchangeName, "", null, message.Serialize());
+            Console.WriteLine("Payment Sent {0}, f{1}", message.CardNumber, message.AmounToPay);
+        }
+
     }
 }
--- PublishSubscribe_Subscriber/Program.cs
+++ PublishSubscribe_Subscriber/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Common;
+using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,44 @@ namespace PublishSubscribe_Subscriber
 {
     class Program
     {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static QueueingBasicConsumer _consumer;
+
+        private const string ExchangeName = "PublishSubscribe_Exchange";
+
         static void Main(string[] args)
         {
+            //create connection factory
+            _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+
+            //create connection and the model
+            using (_connection = _factory.CreateConnection())
+            {
+                using (var channel = _connection.CreateModel())
+                {
+                    var queueName = DeclareAndBindQueueToExchange(channel);
+                    channel.BasicConsume(queueName.ToString(), true, _consumer);  //We use auto ack as we do not need to tell the Exchange we processed the message in a fanout topology.
+
+                    //Dequeue messages forever
+                    while(true)
+                    {
+                        var ea = _consumer.Queue.Dequeue();
+                        var message = (Payment)ea.Body.DeSerialize(typeof(Payment));
+
+                        Console.WriteLine("-----Payment Processed {0} : {1}", message.CardNumber, message.AmounToPay);
+                    }
+                }
+            }
+        }
+
+        private static object DeclareAndBindQueueToExchange(IModel channel)
+        {
+            channel.ExchangeDeclare(ExchangeName, "fanout"); //Declare an exchange.  If it exists it will not be overwritten
+            var queueName = channel.QueueDeclare().QueueName; //We get a queue name 
+            channel.QueueBind(queueName, ExchangeName, ""); //We bind the queue name to the exchange
+            _consumer = new QueueingBasicConsumer(channel); //We create our QueueingBasicConsumer
+            return queueName;
         }
     }
 }
